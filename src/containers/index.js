40b5a81b492531720a2d17177eace55365747541
rayalienkiss <alien1983gs@126.com>
2016-11-22 00:26:57 +0800

    update

--- src/containers/index.js
+++ src/containers/index.js
@@ -18,7 +18,9 @@ export default class Home extends Component {
         return (
             <div className="container-inner">
                 <Header title="供应链云平台" linkTo="" headCls="header index-header"></Header>
-                <div className="test"></div>
+                <div className="index-banner">
+                    <img src="../src/public/images/bitmap/bitmap_1x1.jpg" alt="index banner" data-src="https://www.qianduan.com/Content/images/index/tu3.jpg"/>
+                </div>
                 <Footer></Footer>
             </div>
         )
