05127ac612f8f09e8b0c9d4c0a37ed1ae2ae6586
rayalienkiss <alien1983gs@126.com>
2016-12-16 14:33:41 +0800

    fixed share

--- src/entry.js
+++ src/entry.js
@@ -23,7 +23,6 @@ var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
   hm.src = "https://hm.baidu.com/hm.js?2acaebc1ef0d040c6f8117602345064b";
-  var s = document.getElementsByTagName("script")[0]; 
+  var s = document.getElementsByTagName("script")[0];
   s.parentNode.insertBefore(hm, s);
 })();
-
